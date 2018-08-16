f12a0fbc0b0d1207e0150200fc848348d436aeff
Robert POFUK <robert.pofuk@rbinternational.com>
2018-08-16 10:21:42 +0200

    Update

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e 
+
 cd ..
 cd docker-os && git pull && ./build.sh && cd ..
 cd docker-java && git pull  && chmod +x *.sh -R && ./build.sh && cd ..
@@ -7,3 +9,5 @@ cd docker-zookeeper && git pull  && chmod +x *.sh -R && ./build.sh && cd ..
 cd docker-kafka-broker&& git pull  && chmod +x *.sh -R && ./build.sh && cd ..
 
 cd aws-image-builder
+
+docker stack deploy --compose-file docker-compose.yaml phone
