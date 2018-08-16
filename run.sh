d0b893f6ad63b1092f3797c4588848ac0a80f7ff
Robert POFUK <robert.pofuk@rbinternational.com>
2018-08-16 09:28:17 +0200

    Initial

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
+apt-get update
+apt-get install -y docker-ce
+docker swarm init
+
+mkdir data
+cd data
+git clone https://github.com/loraneo/docker-os.git
+git clone https://github.com/loraneo/docker-java.git
+git clone https://github.com/loraneo/docker-zookeeper.git
+git clone https://github.com/loraneo/docker-kafka-broker.git
+git clone https://github.com/loraneo/aws-image-builder.git
+
+cd docker-os && ./build.sh && cd ..
+cd docker-java && ./build.sh && cd ..
