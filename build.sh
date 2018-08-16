0bc19cc9d34afe240e12423a6c147b3e24119781
Robert POFUK <robert.pofuk@rbinternational.com>
2018-08-16 15:11:08 +0200

    Fix build

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ cd ..
 cd docker-os && git pull && ./build.sh && cd ..
 cd docker-java && git pull  && chmod +x *.sh -R && ./build.sh && cd ..
 cd docker-zookeeper && git pull  && chmod +x *.sh -R && ./build.sh && cd ..
-cd docker-kafka-broker&& git pull  && chmod +x *.sh -R && ./build.sh && cd ..
+cd docker-kafka-broker && git pull  && chmod +x *.sh -R && ./build.sh && cd ..
+cd cdr-gen && git pull  && chmod +x *.sh -R && ./build.sh && cd ..
 
 cd aws-image-builder
 
--- run.sh
+++ run.sh
@@ -19,6 +19,8 @@ apt-get update
 apt-get install -y docker-ce
 docker swarm init
 
+apt-get install maven
+
 mkdir data
 cd data
 git clone https://github.com/loraneo/docker-os.git
@@ -26,9 +28,12 @@ git clone https://github.com/loraneo/docker-java.git
 git clone https://github.com/loraneo/docker-zookeeper.git
 git clone https://github.com/loraneo/docker-kafka-broker.git
 git clone https://github.com/loraneo/aws-image-builder.git
+git clone https://github.com/loraneo/cdr-gen.git
 
 
 cd docker-os && ./build.sh && cd ..
 cd docker-java && chmod +x *.sh -R && ./build.sh && cd ..
 cd docker-zookeeper && chmod +x *.sh -R && ./build.sh && cd ..
 cd docker-kafka-broker && chmod +x *.sh -R && ./build.sh && cd ..
+cd cdr-gen && chmod +x *.sh -R && ./build.sh && cd ..
+
